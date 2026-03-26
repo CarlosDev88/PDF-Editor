39ca47b8dcddb80684064ca8c998f56523bfbd9e
carlos dev 88 <arturorueda88@gmail.com>
2026-03-26 10:36:42 -0500

    readme del proyecto

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-# import eel
-# import sys
-# import app.controllers.api 
-
-# def start_app():
-#     print("Iniciando el motor de Python para OfflinePDF Master...")
-#     eel.init('frontend/dist') 
-    
-#     try:
-#         print("Conectando con el frontend de React en el puerto 5173...")
-#         eel.start(
-#             {'port': 5173}, 
-#             host='localhost', 
-#             port=8888,
-#             cmdline_args=['--start-maximized'], 
-#             disable_cache=True 
-#         )
-#     except (SystemExit, MemoryError, KeyboardInterrupt):
-#         print("Aplicación cerrada limpiamente.")
-
-# if __name__ == '__main__':
-#     start_app()
-
-
 
 import eel
 import sys
